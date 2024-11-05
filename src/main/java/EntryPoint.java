e1b53f6a406d8c5e58072adbee16b3f8efdf366b
KAISAR TOKTARKOZHA <kaisar403575@gmail.com>
2024-11-05 17:22:26 +0500

    deleted dublicats

--- src/main/java/EntryPoint.java
+++
@@ -1,9 +0,0 @@
-import console.UserConsole;
-
-public class EntryPoint {
-    public static void main(String[] args) {
-        UserConsole console = new UserConsole();
-        console.start();
-    }
-}
-
