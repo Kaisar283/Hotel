44f4735a154465c05c2203f2ecb8393ab1aa8c5e
KAISAR TOKTARKOZHA <kaisar403575@gmail.com>
2024-11-16 09:02:39 +0500

    refactor: add User new fields - userRole, createdAt, updatedAt

---
+++ src/main/java/kz/andersen/java_intensive_13/enums/UserRole.java
@@ -0,0 +1,6 @@
+package kz.andersen.java_intensive_13.enums;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
--- src/main/java/kz/andersen/java_intensive_13/models/User.java
+++ src/main/java/kz/andersen/java_intensive_13/models/User.java
@@ -1,7 +1,16 @@
 package kz.andersen.java_intensive_13.models;
 
+import kz.andersen.java_intensive_13.enums.UserRole;
+
+import java.time.ZonedDateTime;
+
 public class User {
+
+    private Long id;
     private String name;
+    UserRole userRole;
+    private ZonedDateTime createdAt;
+    private ZonedDateTime updatedAt;
 
     public User() {
     }
@@ -10,12 +19,52 @@ public class User {
         this.name = name;
     }
 
+    public User(long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.userRole = UserRole.USER;
+        this.createdAt = ZonedDateTime.now();
+        this.updatedAt = ZonedDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(ZonedDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public ZonedDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(ZonedDateTime updatedAt) {
+        this.updatedAt = updatedAt;
     }
 
     @Override
